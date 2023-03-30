b3e31da5bd91003751ec513b73bb2e44987bf1fb
YizhePKU <yizhe@pku.edu.cn>
2023-03-30 18:18:19 +0800

    Checkpoint for debugging

---
+++ src/lib.rs
@@ -0,0 +1,200 @@
+#![no_std]
+
+mod data;
+mod utils;
+
+extern crate alloc;
+
+use alloc::{borrow::ToOwned, collections::BTreeMap, sync::Arc, vec::Vec};
+// use boringtun::Tunn;
+#[derive(Debug)]
+struct Tunn;
+use kernel_alloc::KernelAlloc;
+use kernel_log::KernelLogger;
+use log::LevelFilter;
+use smoltcp::wire::{IpProtocol, Ipv4Address, Ipv4Packet, TcpPacket, UdpPacket};
+use spin::Mutex;
+
+use crate::data::{Appid, Connection};
+use crate::utils::make_tunn;
+
+#[global_allocator]
+static ALLOCATOR: KernelAlloc = KernelAlloc;
+
+// These functions are required to shut the linker up.
+// Probably related to linker, panic, abort, exceptions, etc.
+#[no_mangle]
+pub extern "C" fn _DllMainCRTStartup() {}
+#[no_mangle]
+pub extern "C" fn __CxxFrameHandler3() {}
+#[no_mangle]
+pub extern "C" fn _fltused() {}
+
+#[panic_handler]
+fn my_panic(info: &core::panic::PanicInfo) -> ! {
+    log::error!("Panic: {}", info);
+    loop {}
+}
+
+// global state
+#[derive(Debug)]
+struct State {
+    pub tunnels: BTreeMap<Appid, Arc<Mutex<Tunn>>>,
+    pub connections: BTreeMap<Connection, Arc<Mutex<Tunn>>>,
+}
+static STATE: Mutex<State> = Mutex::new(State {
+    tunnels: BTreeMap::new(),
+    connections: BTreeMap::new(),
+});
+
+#[no_mangle]
+pub extern "C" fn rsInit() {
+    log::info!("rsInit entry");
+
+    KernelLogger::init(LevelFilter::Info).expect("Failed to initialize logger");
+
+    // Setup tracked processes
+    // Hard-coded appid for now
+    let filename = r"\device\harddiskvolume4\program files (x86)\nmap\ncat.exe";
+    let appid = Appid::from_u16_slice(&[
+        92, 100, 101, 118, 105, 99, 101, 92, 104, 97, 114, 100, 100, 105, 115, 107, 118, 111, 108,
+        117, 109, 101, 52, 92, 112, 114, 111, 103, 114, 97, 109, 32, 102, 105, 108, 101, 115, 32,
+        40, 120, 56, 54, 41, 92, 110, 109, 97, 112, 92, 110, 99, 97, 116, 46, 101, 120, 101, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+
+    // Create tunnels and associate them with appid
+    STATE.lock().tunnels.insert(
+        appid,
+        Arc::new(Mutex::new(todo!())),
+        // Arc::new(Mutex::new(make_tunn("privkey", "pubkey", 0))),
+    );
+
+    log::info!("rsInit exit");
+}
+
+#[no_mangle]
+pub extern "C" fn rsIsAppTracked(appid: *const u8, appid_size: u32) -> bool {
+    let appid = Appid::new(appid, appid_size);
+    STATE.lock().tunnels.contains_key(&appid)
+}
+
+#[no_mangle]
+pub extern "C" fn rsRegisterConnection(
+    appid: *const u8,
+    appid_size: u32,
+    protocol: u8,
+    local_addr: u32,
+    local_port: u16,
+    remote_addr: u32,
+    remote_port: u16,
+) {
+    // Skip untracked apps
+    if !rsIsAppTracked(appid, appid_size) {
+        return;
+    }
+
+    let appid = Appid::new(appid, appid_size);
+    let connection = Connection {
+        protocol: if protocol == 6 {
+            IpProtocol::Tcp
+        } else {
+            IpProtocol::Udp
+        },
+        local_addr: Ipv4Address::from_bytes(&local_addr.to_be_bytes()),
+        local_port,
+        remote_addr: Ipv4Address::from_bytes(&remote_addr.to_be_bytes()),
+        remote_port,
+    };
+    // Associate the connection with a tunnel, which should exist, since `rsIsAppTracked` already checks for tracking status.
+    // There is no "time-of-check to time-of-use" bug since tunnels never changes after initialization.
+    let tunn = STATE.lock().tunnels.get(&appid).unwrap().clone();
+    STATE.lock().connections.insert(connection, tunn);
+    log::info!(
+        "New connection: {}, {}:{} --> {}:{}",
+        appid,
+        connection.local_addr,
+        connection.local_port,
+        connection.remote_addr,
+        connection.remote_port
+    );
+}
+
+#[no_mangle]
+pub extern "C" fn rsHandleInboundPacket(buf: *mut u8, size: u32) -> bool {
+    let data = unsafe { core::slice::from_raw_parts_mut(buf, size as usize) };
+
+    // log::info!("rsHandleInboundPacket entry. data: {data:?}");
+    true
+}
+
+#[no_mangle]
+pub extern "C" fn rsHandleOutboundPacket(buf: *mut u8, size: u32) -> bool {
+    let buffer = unsafe { core::slice::from_raw_parts(buf, size as usize) };
+
+    // Parse packet
+    // TODO: Surely there's a cleaner way to handle all these match branches...
+    let ipv4_packet = match Ipv4Packet::new_checked(buffer) {
+        Err(_) => {
+            return true;
+        }
+        Ok(packet) => {
+            if packet.version() != 4 {
+                return true;
+            }
+            packet
+        }
+    };
+    let protocol = ipv4_packet.next_header();
+    let (connection, payload) = match protocol {
+        IpProtocol::Tcp => match TcpPacket::new_checked(ipv4_packet.payload()) {
+            Err(_) => {
+                return true;
+            }
+            Ok(tcp_packet) => {
+                let connection = Connection {
+                    protocol,
+                    local_addr: ipv4_packet.src_addr(),
+                    local_port: tcp_packet.src_port(),
+                    remote_addr: ipv4_packet.dst_addr(),
+                    remote_port: tcp_packet.dst_port(),
+                };
+                let payload = tcp_packet.payload();
+                (connection, payload)
+            }
+        },
+        IpProtocol::Udp => match UdpPacket::new_checked(ipv4_packet.payload()) {
+            Err(_) => {
+                return true;
+            }
+            Ok(udp_packet) => {
+                let connection = Connection {
+                    protocol,
+                    local_addr: ipv4_packet.src_addr(),
+                    local_port: udp_packet.src_port(),
+                    remote_addr: ipv4_packet.dst_addr(),
+                    remote_port: udp_packet.dst_port(),
+                };
+                let payload = udp_packet.payload();
+                (connection, payload)
+            }
+        },
+        _ => {
+            return true;
+        }
+    };
+
+    // Check if the connection is tracked, and if so, which tunnel it is associated with
+    let tunn = match STATE.lock().connections.get_mut(&connection) {
+        None => {
+            return true;
+        }
+        Some(tunn) => tunn,
+    };
+
+    // Encrypt the payload and reconstruct the packet.
+    // TODO
+    log::info!("Trying to encrypt payload for connection {connection}");
+    return false;
+}
---
+++ src/utils.rs
@@ -0,0 +1,53 @@
+use base64::Engine;
+use boringtun::Tunn;
+
+/// Decodes a base64-encoded string into 32 bytes of data.
+fn decode_key(base64: &str) -> Result<[u8; 32], &'static str> {
+    let b64 = base64::engine::general_purpose::STANDARD;
+    let mut buf = [0; 40]; // need a buffer slightly larger than 32 for decoding
+    match b64.decode_slice(base64, &mut buf) {
+        Ok(len) => {
+            if len != 32 {
+                Err("Data not 32 bytes")
+            } else {
+                // take the first 32 bytes of buf
+                let data = <[u8; 32]>::try_from(&buf[..32]).unwrap();
+                Ok(data)
+            }
+        }
+        Err(_) => Err("Base64 string too long"),
+    }
+}
+
+/// Create a point-to-point tunnel with a peer.
+///
+/// [`privkey`] is the base64-encoded private key of the local interface.
+/// [`pubkey`] is the base64-encoded public key of the peer.
+/// [`peer_id`] is a unique integer for the peer.
+///
+/// Typical usage is multiple [`Tunn`] instances with the same [`privkey`],
+/// different [`pubkey`], and an incrementing [`peer_id`].
+///
+/// # Panics
+///
+/// Panic if [`peer_id`] cannot fit in a 24 bytes integer (which is 16M active peers).
+pub fn make_tunn(privkey: &str, pubkey: &str, peer_id: u32) -> Tunn {
+    if peer_id & 0xffffff != peer_id {
+        panic!("Only 2^24 peers are supported at a time");
+    }
+
+    let static_private = x25519_dalek::StaticSecret::from(decode_key(privkey).unwrap());
+    let peer_static_public = x25519_dalek::PublicKey::from(decode_key(pubkey).unwrap());
+    // TODO: support persistent keepalive
+    // The first 24 bits of index are used to identify a peer, and the rest 8 bits are for session rotations
+    let tunn = Tunn::new(
+        static_private,
+        peer_static_public,
+        None,
+        None,
+        peer_id << 8,
+        None,
+    )
+    .unwrap();
+    return tunn;
+}
